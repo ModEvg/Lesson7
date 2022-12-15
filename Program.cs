ca730a268ff024a9c3b5792036e4373a1cf53a79
ModinaEN <modinaen@gmail.com>
2022-12-15 09:07:13 +0300

    умножено на 10

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ for (int i=0; i<array.GetLength(0); i++)
     {
         for (int j=0;j<array.GetLength(1); j++)
         {
-            array[i, j] = Math.Round(new Random().NextDouble(), 1);
+            array[i, j] = Math.Round(new Random().NextDouble()*10, 1);
         }
     }
 PrintArray(array);
